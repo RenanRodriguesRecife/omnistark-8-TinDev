73cef3b86044fa0d4e828d8e300c61e2de53ab1a
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-21 17:34:03 -0300

    add mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const routes = require('./routes');
 const server = express();
 
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0.iqw1b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
 server.use(express.json());
 server.use(routes);
