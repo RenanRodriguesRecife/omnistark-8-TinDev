01959e049999a061197c908cd67894804d03441b
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-22 20:59:12 -0300

    install cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+// Permite que a aplicação seja acessada por qualquer indereço
+const cors = require('cors');
 const routes = require('./routes');
 const server = express();
+
+
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0.iqw1b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
     useNewUrlParser:true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
