0103e12f7f1d3f2c423d219fcc7c3d60b01c78ca
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-22 20:56:15 -0300

    list devs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,6 +10,7 @@ routes.get('/',(req,res)=>{
     // return res.send(`Hello ${req.query.name}`);
 })
 
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 routes.post('/devs/:devId/likes', LikeController.store);
 routes.post('/devs/:devId/dislikes', DislikeController.store);
