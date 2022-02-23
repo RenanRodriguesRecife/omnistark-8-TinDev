e0895fb8fff6acf1f41a9ada78b47ba3ce15d982
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-23 13:31:08 -0300

    add logo img

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import './Login.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
+    <>
+   <img src={logo} alt="Tindev"/>
    <h1> Tindev</h1>
+   </>
   );
 }
 
