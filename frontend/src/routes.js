6142a07f0293583d6f02ff287c575c5270f73158
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-23 21:54:16 -0300

    react router atualizado

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+
 
 import Login from './pages/Login';
 import Main from './pages/Main';
 
-export default function Routes() {
-  return (
-    <BrowserRouter>
-      <Route path="/" exact component={Login} />
-      <Route path="/dev/:id" component={Main} />
-    </BrowserRouter>
-  );
+export default function Rout(){
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" exact element={<Login/>}/>
+            <Route path="/main" element={<Main/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
