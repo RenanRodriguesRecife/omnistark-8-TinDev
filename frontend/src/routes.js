621deafab92d0eddcb823a4624a041878cd3d8a7
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-25 14:10:05 -0300

    adapt the code to the old version of react-router-dom

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Main from './pages/Main';
 
-export default function Rout(){
-    return(
-        <BrowserRouter>
-        <Routes>
-            <Route path="/" exact element={<Login/>}/>
-            <Route path="/main" element={<Main/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Login} />
+      <Route path="/main" component={Main} />
+    </BrowserRouter>
+  );
 }
