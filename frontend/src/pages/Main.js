18d8645cb7327b48efe6cb03d659eaf4fc4b4135
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-26 18:22:42 -0300

    base html page

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
-export default function Main(){
-    return <h1>Hello World</h1>
+import logo from '../assets/logo.svg';
+import dislike from '../assets/dislike.svg';
+import like from '../assets/like.svg';
+
+
+
+export default function Main({match}){
+    return (
+        <div className="main-container">
+            <img src={logo} alt="Tindev"/>
+            <ul>
+                <li>
+                    <img src="teste" alt=""/>
+                    <footer>
+                        <strong>NOme</strong>
+                        <p>descricao</p>
+                    </footer>
+                    <div className='buttons'>
+                        <button type="button">
+                            <img src={dislike} alt="Dislike"/>
+                        </button>
+                        <button type="button">
+                            <img src={like} alt="Like"/>
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
 }
