28afd12fbd68d70e59fea29d13e55645d2d0ce6b
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-27 01:37:04 -0300

    like and dislike handle

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -23,10 +23,28 @@ export default function Main({match}){
         loadUsers();
     },[match.params.id])
 
+    async function handleLike(id){
+        console.log('like',id)
+        await api.post(`/devs/${id}/likes`,null, {
+            headers: {user: match.params.id}
+        })
+
+        setUsers(users.filter(user => user._id !== id));
+    }
+
+    async function handleDislike(id){
+        console.log('dislike',id)
+        await api.post(`/devs/${id}/dislikes`, null, {
+            headers: {user: match.params.id}
+        })
+        setUsers(users.filter(user => user._id !== id));
+    }
+
     return (
         <div className="main-container">
             <img src={logo} alt="Tindev"/>
-            <ul>
+            { users.length > 0 ? (
+                <ul>
                 {users.map(user => (
                     <li key={user._id}>
                         <img src={user.avatar} alt={user.name}/>
@@ -35,17 +53,21 @@ export default function Main({match}){
                             <p>{user.bio}</p>
                         </footer>
                         <div className='buttons'>
-                            <button type="button">
-                                <img src={dislike} alt="Dislike"/>
+                            {/* hecker para executar a função e passar o parametro */}
+                            <button type="button" onClick={() => handleDislike(user._id)}>
+                                <img src={dislike} alt="Dislike" />
                             </button>
-                            <button type="button">
-                                <img src={like} alt="Like"/>
+                            <button type="button" onClick={() => handleLike(user._id)}>
+                                <img src={like} alt="Like" />
                             </button>
                         </div>
                     </li>
                 ))}
                 
             </ul>
+            ) : (
+                <div className='empty'>Acabou :(</div>
+            )}
         </div>
     )
 }
